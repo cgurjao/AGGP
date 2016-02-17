b6ddfb5494be11f4113ae8ecbd8e5d7e4f2dda4a
ArthurPERE <arthur.pere@insa-lyon.fr>
2016-02-17 11:11:12 +0100

    end with graphic representation

--- clique.py
+++ clique.py
@@ -1,9 +1,43 @@
 import networkx as nx
 import random
+import matplotlib.pyplot as plt
+
 genome = [[random.randint(0,1) for x in range(10)] for x in range(10)]
+
 for i in genome:
 	print i
 
-G = nx.graph()
+G = nx.Graph()
 G.add_nodes_from(range(10))
 
+liste_edges = []
+for i, vi in enumerate(genome[0]):
+	for j, vj in enumerate(genome[0]):
+		if i != j:
+			if vi == 1 or vj == 1:
+				liste_edges.append( (i,j) )
+
+print liste_edges
+
+
+G.add_edges_from(liste_edges)
+
+
+pos = nx.spring_layout(G) # position of all nodes
+
+
+# draw everything on the graph without any labels
+plt.figure(1)
+nx.draw(G,pos=nx.spring_layout(G))
+plt.draw()
+
+# to draw step by step the graph
+plt.figure(2)
+nx.draw_networkx_nodes(G,pos)
+
+nx.draw_networkx_edges(G,pos, width=6, alpha=0.5, edge_color='b', style='dashed')
+nx.draw_networkx_labels(G,pos, font_size=20, font_family='sans-serif')
+
+
+
+plt.show()
