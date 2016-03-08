a657b7eaa307ac06c66b5560dec8975d3a96b46a
ArthurPERE <arthur.pere@insa-lyon.fr>
2016-03-08 17:29:46 +0100

    define the class genome

--- genome.py
+++ genome.py
@@ -1,12 +1,43 @@
 import networkx as nx
 import numpy as np
 import matplotlib as np
+import numpy.random as r
 
 
-# The number of the genes we want
-nb_gene = 10
 
-# to create the random matrix for the initialisation
-genome = np.reshape(np.random.random_integers(0,1,size=n**2),(n,n))
-# print genome
+class genome:
+	"""docstring for genome"""
+	def __init__(self, number_gene, p_ponctual_mutation, p_inversion_mutation, p_Xover_mutation, p_mutation):
+		
+		# The number of the genes we want
+		self.nb_gene = number_gene
 
+		# to create the random matrix for the initialisation, full of 0 and 1
+		self.genome = r.randint(2,size=(nb_gene,nb_gene))
+		# print genome
+
+		# the differente probability for the mutations
+		self.ponctual = p_ponctual_mutation
+		self.inversion = p_inversion_mutation
+		self.Xover = p_Xover_mutation
+
+		# the probability to mutate
+		self.mutation = p_mutation
+
+
+	####################################################
+	# function to mutate the genome
+	####################################################
+	def MutateGenome(self, fitness):
+
+		# define the formula to associate the fitness to the number of mutation
+		nb_mutation = int(fitness * 5)
+
+		for i in xrange(nb_mutation):
+
+			# choose one gene among the others genes (will it be one mutation on one gene or can we allow several mutations
+			# on one genes ?)
+			n_gene = r.randint(self.nb_gene)
+			gene = self.genome[n_gene,:]
+
+			
