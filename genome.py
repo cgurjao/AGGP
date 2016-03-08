b5722a7a2fc1f5f35cbeb34fa4acad50ba191261
ArthurPERE <arthur.pere@insa-lyon.fr>
2016-03-08 16:57:58 +0100

    first commit of genome

---
+++ genome.py
@@ -0,0 +1,12 @@
+import networkx as nx
+import numpy as np
+import matplotlib as np
+
+
+# The number of the genes we want
+nb_gene = 10
+
+# to create the random matrix for the initialisation
+genome = np.reshape(np.random.random_integers(0,1,size=n**2),(n,n))
+# print genome
+
