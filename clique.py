f4e5b237852a6097108f8dd4832f03dfabfa6c9d
Gurjao Carino <cgurjao@bs406-s31-06.insa-lyon.fr>
2016-03-11 09:39:07 +0100

    Correct compararision of expected and observed distribution of degree proportion

--- clique.py
+++ clique.py
@@ -2,20 +2,22 @@ import networkx as nx
 from random import *
 import numpy as numpy
 import math
-
-N = 100#Define number of nodes
-gamma = 2.2
 try:
     import matplotlib.pyplot as plt
 except:
     raise
+
+##Define number of nodes
+N = 500
+##Scale-free network parameters
+gamma = 2.2
+
 ###########################################
 ######### Generate random genome ##########
 ###########################################
 ##Genome with random numbers
 genome = numpy.reshape(numpy.random.random_integers(0,1,size=N*N),(N,N))
 ##To symmetrize the matrix
-#genome = genome + genome.T - numpy.diag(genome.diagonal())
 genome = (genome + genome.T)/2
 ##Put all non value to 1 and fill the diagonal
 for i in xrange(N):
@@ -25,7 +27,7 @@ for i in xrange(N):
 		if i == j:
 			genome[i][j] = 1
 		
-#Custom genome for tests
+##Custom genome for tests
 #genome = [[1, 0, 0, 0, 0],[0, 1, 0, 1, 1],[0, 0, 1, 1, 1],[0, 1, 1, 1, 0],[0, 1, 1, 0, 1]]
 
 
@@ -51,18 +53,20 @@ for i in xrange(N):
 #print G.number_of_edges()
 #print G.edges()
 
-#pos = nx.spring_layout(G)
-#nx.draw(G, pos)
-#node_labels = nx.get_node_attributes(G,'state')
-#nx.draw_networkx_labels(G, pos, labels = node_labels)
+##Uncomment to draw graph
+'''
+pos = nx.spring_layout(G)
+nx.draw(G, pos)
+node_labels = nx.get_node_attributes(G,'state')
+nx.draw_networkx_labels(G, pos, labels = node_labels)
+##Uncomment to save figure
 #plt.savefig("simple_path.png")
-#plt.show() 			
+plt.show() 			
+'''
 
 ###########################################
 ######## Average of shortest path #########
 ###########################################
-## It is the sum of all shortest paths divided by the number of paths (which is n!)
-
 def average_shortest(g):
 	avg = 0.0
 	for node in g:
@@ -83,47 +87,103 @@ print "Average of all shortest paths is %f \n" %avg
 ###########################################
 ########## Manipulate degrees #############
 ###########################################
+degree_sequence = list(G.degree().values()) - numpy.ones(len(G.degree().values()))
 
+## Check-up for tests
 #print G.edges()
 #print G.degree(0)
-degree_sequence = list(G.degree().values()) - numpy.ones(len(G.degree().values()))
 #print(degree_sequence)
 
-degree_sequence=sorted(nx.degree(G).values(),reverse=True) # degree sequence
-#print "Sorted degree sequence", degree_sequence
-#dmax=max(degree_sequence)
 
+##Sort by degrees
+degree_sequence_sorted=sorted(degree_sequence,reverse=True)
+#print "Degree sequence"
+#print degree_sequence_sorted
+
+##Uncomment to draw histogram of degrees with graph on the same figure
+"""
 plt.plot(degree_sequence,'b-',marker='o')
 plt.title("Degree rank plot")
 plt.ylabel("degree")
 plt.xlabel("rank")
-
-# draw graph in inset
+## draw graph in inset
 plt.axes([0.45,0.45,0.45,0.45])
 Gcc=sorted(nx.connected_component_subgraphs(G), key = len, reverse=True)[0]
 pos=nx.spring_layout(Gcc)
 plt.axis('off')
 nx.draw_networkx_nodes(Gcc,pos,node_size=20)
 nx.draw_networkx_edges(Gcc,pos,alpha=0.4)
-
+##Uncomment to save figure
 #plt.savefig("degree_histogram.png")
 plt.show()
+"""
+
+##Uncomment to draw figure with expected and observed curve of proportions of each degree
+'''
+plt.plot(degree_sequence,'b-',marker='o')
+plt.title("Degree rank plot")
+plt.ylabel("degree")
+plt.xlabel("rank")
+## draw graph in inset
+plt.axes([0.45,0.45,0.45,0.45])
+Gcc=sorted(nx.connected_component_subgraphs(G), key = len, reverse=True)[0]
+pos=nx.spring_layout(Gcc)
+plt.axis('off')
+nx.draw_networkx_nodes(Gcc,pos,node_size=20)
+nx.draw_networkx_edges(Gcc,pos,alpha=0.4)
+##Uncomment to save figure
+#plt.savefig("degree_histogram.png")
+plt.show()
+'''
 
 RSS = 0.0
-for i in xrange(len(degree_sequence)):
-	observed = float(float(degree_sequence.count(degree_sequence[i]))/float(len(degree_sequence)))
-	expected = math.pow(degree_sequence[i], -gamma)
-	RSS = RSS + abs(observed - expected)
+observed = []
+expected = []
+##Sort by unique degrees
+unique_degrees=sorted(set(degree_sequence),reverse=True)
+#print unique_degrees
+
+## Calculate observed and expected values of proportions of each degree
+for i in xrange(len(unique_degrees)):
+	observed.append(float(float(degree_sequence_sorted.count(unique_degrees[i]))))
+	expected.append(math.pow(unique_degrees[i], -gamma))
+	RSS = RSS + abs(observed[i] - expected[i])
+
+
+##To calculate proportionnality coefficient between observed and expected values
+ratio = 0.0
+for i in xrange(len(unique_degrees)):
+	ratio = ratio + (observed[i]/expected[i])
+
+ratio = ratio/len(unique_degrees)
+print "Ratio"
+print ratio
+	
+for i in xrange(len(unique_degrees)):
+	expected[i] = expected[i]*ratio
+
+##Uncomment to draw figure with expected and observed curve of proportions of each degree
+plt.plot(unique_degrees, observed,marker='o')
+plt.plot(unique_degrees, expected,marker='o')
+plt.title("Degree proportion")
+plt.ylabel("Proportion")
+plt.xlabel("Degree")
+plt.show()
+## draw graph in inset
+'''plt.axes([0.45,0.45,0.45,0.45])
+Gcc=sorted(nx.connected_component_subgraphs(G), key = len, reverse=True)[0]
+pos=nx.spring_layout(Gcc)
+plt.axis('off')
+nx.draw_networkx_nodes(Gcc,pos,node_size=20)
+nx.draw_networkx_edges(Gcc,pos,alpha=0.4)
+##Uncomment to save figure
+#plt.savefig("degree_histogram.png")
+'''
 
 print "Score Scale-free!"
 print "Root Sum Square is %f \n" %RSS
 
-'''############################################
-# To have the frequence list
-############################################
-list_deg = nx.degree(G).values()
-freq = {}
-for i in set(list_deg):
-	freq[i] = list_deg.count(i)
-print freq'''
+###########################################
+########## Manipulate cliques #############
+###########################################
 
