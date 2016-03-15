d035015d924552eb46eee4faf4631f83e61a3e0c
ArthurPERE <arthur.pere@insa-lyon.fr>
2016-03-15 17:35:48 +0100

    add few graphic modification

--- genome.py
+++ genome.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 import numpy.random as r
 
 class genome:
-	"""docstring for genome"""
 	def __init__(self, number_gene):
 		
 		# The number of the genes we want
@@ -18,6 +17,17 @@ class genome:
 			for j in xrange(i):
 				self.genome[i,j] = self.genome[j,i]
 
+
+		# to construct the graph
+		self.G = nx.Graph()
+		self.G.add_nodes_from(range(self.nb_gene))
+
+		for i in xrange(self.nb_gene):
+			for j in xrange(i+1):
+				if self.genome[i,j] == 1:
+					self.G.add_edge(i,j)
+
+		self.position = nx.circular_layout(self.G)
 		
 
 	####################################################
@@ -31,42 +41,35 @@ class genome:
 					if r.random() < proba_matrix[i,j]:
 						self.genome[i,j] = 1
 						self.genome[j,i] = 1
+						self.G.add_edge(i,j)
 		
 				if proba_matrix[i,j] < 0:
 					if r.random() < -proba_matrix[i,j]:
 						self.genome[i,j] = 0
 						self.genome[i,j] = 0
-
+						if (i,j) in self.G.edges() or (j,i) in self.G.edges():
+							self.G.remove_edge(i,j)
 
 
 	####################################################
 	# function to draw the graph and show the graph
 	####################################################
 	def draw(self):
-		G = nx.Graph()
-		G.add_nodes_from(range(self.nb_gene))
+		plt.figure()
+		list_deg = np.array(nx.degree(self.G).values())
 
-		for i in xrange(self.nb_gene):
-			for j in xrange(i+1):
-				if self.genome[i,j] == 1:
-					G.add_edge(i,j)
-
-
-
-		G0 = sorted(nx.connected_component_subgraphs(G), key = len, reverse=True)[0]
-
-
-
-		list_deg = np.array(nx.degree(G).values())
-		position = nx.circular_layout(G)
-
-		nx.draw_networkx_nodes(G, pos=position, node_size=100*list_deg, alpha=0.4)
-		nx.draw_networkx_edges(G, pos=position, alpha=0.2)
-		nx.draw_networkx_labels(G,pos=position)
+		nx.draw_networkx_nodes(self.G, pos=self.position, node_size=300, alpha=0.5, node_color=list_deg, cmap=plt.cm.YlOrRd)# RdPu, BuPu
+		# http://matplotlib.org/examples/color/colormaps_reference.html
+		nx.draw_networkx_edges(self.G, pos=self.position, alpha=0.2)
+		nx.draw_networkx_labels(self.G,pos=self.position)
 		plt.draw()
-		plt.show()
 
 
+n = 100
+a = r.rand(n,n) * r.choice([-1, 1], size=(n,n))
+G = genome(n)
+G.draw()
+G.UpdateMatrix(a)
 
-genome(10).draw()
-
+G.draw()
+plt.show()
