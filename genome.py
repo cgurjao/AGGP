00457b362f89b39145b4a68c7f81e73f3c5491bc
ArthurPERE <arthur.pere@insa-lyon.fr>
2016-03-15 19:05:59 +0100

    add a save for graphique

--- genome.py
+++ genome.py
@@ -16,18 +16,7 @@ class genome:
 		for i in xrange(self.nb_gene):
 			for j in xrange(i):
 				self.genome[i,j] = self.genome[j,i]
-
-
-		# to construct the graph
-		self.G = nx.Graph()
-		self.G.add_nodes_from(range(self.nb_gene))
-
-		for i in xrange(self.nb_gene):
-			for j in xrange(i+1):
-				if self.genome[i,j] == 1:
-					self.G.add_edge(i,j)
-
-		self.position = nx.circular_layout(self.G)
+			self.genome[i,i] = 1
 		
 
 	####################################################
@@ -36,40 +25,55 @@ class genome:
 	def UpdateMatrix(self, proba_matrix):
 		for i in xrange(self.nb_gene):
 			for j in xrange(i):
-
-				if proba_matrix[i,j] > 0:
-					if r.random() < proba_matrix[i,j]:
-						self.genome[i,j] = 1
-						self.genome[j,i] = 1
-						self.G.add_edge(i,j)
-		
 				if proba_matrix[i,j] < 0:
-					if r.random() < -proba_matrix[i,j]:
-						self.genome[i,j] = 0
-						self.genome[i,j] = 0
-						if (i,j) in self.G.edges() or (j,i) in self.G.edges():
-							self.G.remove_edge(i,j)
+					self.genome[i,j], self.genome[j,i] = abs(self.genome[i,j]-1), abs(self.genome[i,j]-1)
+
+				if proba_matrix[i,j] == 0:
+					if r.rand() < 0.5:
+						self.genome[i,j], self.genome[j,i] = abs(self.genome[i,j]-1), abs(self.genome[i,j]-1)
+
 
 
 	####################################################
-	# function to draw the graph and show the graph
+	# function to draw the graph
 	####################################################
-	def draw(self):
-		plt.figure()
-		list_deg = np.array(nx.degree(self.G).values())
+	def draw(self, itteration=False, save=False):
+		# if you use the option save = True, you will save also all the past graph but not the future graph
 
-		nx.draw_networkx_nodes(self.G, pos=self.position, node_size=300, alpha=0.5, node_color=list_deg, cmap=plt.cm.YlOrRd)# RdPu, BuPu
+		# to construct the graph
+		G = nx.Graph()
+		G.add_nodes_from(range(self.nb_gene))
+
+		for i in xrange(self.nb_gene):
+			for j in xrange(i+1):
+				if self.genome[i,j] == 1:
+					G.add_edge(i,j)
+
+		position = nx.circular_layout(G)
+
+
+		fig = plt.figure()
+
+		list_deg = np.array(nx.degree(G).values())
+
+		nx.draw_networkx_nodes(G, pos=position, node_size=300, alpha=0.5, node_color=list_deg, cmap=plt.cm.YlOrRd)# RdPu, BuPu
 		# http://matplotlib.org/examples/color/colormaps_reference.html
-		nx.draw_networkx_edges(self.G, pos=self.position, alpha=0.2)
-		nx.draw_networkx_labels(self.G,pos=self.position)
+		nx.draw_networkx_edges(G, pos=position, alpha=0.2)
+		nx.draw_networkx_labels(G,pos=position)
 		plt.draw()
 
+		if itteration != False:
+			plt.title('graph for itteration : %d'%itteration)
 
-n = 100
+			if save == True:
+				plt.savefig('graph_itteration_%d'%itteration)
+
+		if save == True:
+			fig.savefig('graph')
+
+n = 10
 a = r.rand(n,n) * r.choice([-1, 1], size=(n,n))
 G = genome(n)
 G.draw()
 G.UpdateMatrix(a)
-
-G.draw()
-plt.show()
+G.draw(1, True)
