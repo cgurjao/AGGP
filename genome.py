c85bc91ca1abbc3f7a3447f07933e4b4ca97a85a
Vattiato Giorgia <gvattiato@bs406-s31-05.insa-lyon.fr>
2016-03-21 10:06:55 +0100

    first sync genome fitness

--- genome.py
+++ genome.py
@@ -2,6 +2,7 @@ import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
 import numpy.random as r
+import fitness
 
 class genome:
 	def __init__(self, number_gene):
@@ -24,24 +25,22 @@ class genome:
 	####################################################
 	# function to update the matrix
 	####################################################
-	def UpdateMatrix(self, proba_matrix):
+	def UpdateMatrix(self, score_matrix):
 		for i in xrange(self.nb_gene):
 			for j in xrange(i):
-				if proba_matrix[j,i] < 0:
+				if score_matrix[j,i] < 0:						# If score is negative (bad) -> mutation!
 					self.genome[j,i] = abs(self.genome[j,i]-1)
 
-				if proba_matrix[j,i] == 0:
+				if score_matrix[j,i] == 0:                      # If score is null-> mutation half of the time
 					if r.rand() < 0.5:
 						self.genome[j,i] = abs(self.genome[i,j]-1)
 
-
+				# If score is positive (good) -> no mutation
 
 	####################################################
 	# function to draw the graph
 	####################################################
-	def draw(self, itteration=False, save=False):
-		# if you use the option save = True, you will save also all the past graph but not the future graph
-
+	def graph(self):
 		# to construct the graph
 		G = nx.Graph()
 		G.add_nodes_from(range(self.nb_gene))
@@ -50,7 +49,13 @@ class genome:
 			for j in xrange(i+1):
 				if self.genome[j,i] == 1:
 					G.add_edge(i,j)
+		return G
 
+
+	def draw(self, graph, itteration=False, save=False):
+		# if you use the option save = True, you will save also all the past graph but not the future graph
+
+		
 		position = nx.circular_layout(G)
 
 
@@ -74,8 +79,20 @@ class genome:
 			plt.title('Graph')
 			plt.savefig('graph')
 
+
+
+
 n = 10
 a = r.rand(n,n) * r.choice([-1, 1], size=(n,n))
-G = genome(n)
+Gen = genome(n)
 
-plt.show()
+#plt.show()
+
+compteur = 0
+
+for i in xrange(10):
+	S0 = fitness.matrix_score(Gen.graph())
+	Gen.UpdateMatrix(S0)
+	compteur +=1
+
+print "\n",Gen.genome
